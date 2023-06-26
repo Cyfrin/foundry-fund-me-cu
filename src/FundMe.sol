00b9f4d9f7bfe20b0031cae9ce6d156211ae8a69
Ethan <96821054+Ethanol48@users.noreply.github.com>
2023-06-26 14:17:50 +0200

    Typo FundMe.sol
    In the video the wasn't a problem with this, I don't know if it's intentional, but I made a PR in case that it was a mistake :)

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -20,7 +20,7 @@ contract FundMe {
     using PriceConverter for uint256;
 
     // State variables
-    uint256 public constant MINIMUM_USD = 50 * 10 ** 18;
+    uint256 public constant MINIMUM_USD = 5 * 10 ** 18;
     address private immutable i_owner;
     address[] private s_funders;
     mapping(address => uint256) private s_addressToAmountFunded;

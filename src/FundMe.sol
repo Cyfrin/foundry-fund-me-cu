4ede710d21b3fe0709db4a75d991f29ec779a82e
Raghunath Ravindra Sali <44742288+raghunath18m@users.noreply.github.com>
2023-07-13 08:48:03 +0530

    Update FundMe.sol
    no expected arguments

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -52,7 +52,7 @@ contract FundMe {
 
     /// @notice Funds our contract based on the ETH/USD price
     function fund() public payable {
-        require(msg.value.getConversionRate(s_priceFeed) >= MINIMUM_USD, "You need to spend more ETH!");
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "You need to spend more ETH!");
         // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
         s_addressToAmountFunded[msg.sender] += msg.value;
         s_funders.push(msg.sender);

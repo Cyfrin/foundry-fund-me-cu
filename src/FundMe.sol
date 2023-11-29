a2bd667586cc0cb28bf691bed06fb851ea7e3e85
thomasender <freisingen@yahoo.com>
2023-11-29 08:16:22 +0100

    update aggregatorv3interface import

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -3,7 +3,6 @@
 pragma solidity ^0.8.19;
 // 2. Imports
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import {PriceConverter} from "./PriceConverter.sol";
 
 // 3. Interfaces, Libraries, Contracts

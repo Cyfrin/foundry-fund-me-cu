6503e7af932e0f88e7e30fbb04059612ea77dcea
Ramandeep <ramansb100@gmail.com>
2024-03-31 23:01:17 +0100

    Update MockV3Aggregator.sol
    Ensure MockV3Aggregator contract explicitly adheres to AggregatorV3Interface.

--- test/mock/MockV3Aggregator.sol
+++ test/mock/MockV3Aggregator.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
 /**
  * @title MockV3Aggregator
  * @notice Based on the FluxAggregator contract
@@ -9,7 +11,7 @@ pragma solidity ^0.8.0;
  * aggregator contract, but how the aggregator got
  * its answer is unimportant
  */
-contract MockV3Aggregator {
+contract MockV3Aggregator is AggregatorV3Interface {
     uint256 public constant version = 4;
 
     uint8 public decimals;

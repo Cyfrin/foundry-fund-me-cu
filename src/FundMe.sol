2d6cf649c9fbb0e8228ebe2496d5d831a6f6522a
Rayyan Uddin <rayyanuddin007@gmail.com>
2024-09-16 19:23:21 +0500

    Update FundMe.sol
    i_owner state variable would be public.

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -21,7 +21,7 @@ contract FundMe {
 
     // State variables
     uint256 public constant MINIMUM_USD = 5 * 10 ** 18;
-    address private immutable i_owner;
+    address public immutable i_owner;
     address[] private s_funders;
     mapping(address => uint256) private s_addressToAmountFunded;
     AggregatorV3Interface private s_priceFeed;
